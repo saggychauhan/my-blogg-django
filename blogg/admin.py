cc473928613c6c81a0036e3b8ca878d0767bd250
saggychauhan <96346937+saggychauhan@users.noreply.github.com>
2022-01-28 13:30:07 +0530

    Add files via upload

--- blogg/admin.py
+++ blogg/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-
 from blogg.models import Post
-
 admin.site.register(Post)
--- blogg/forms.py
+++ blogg/forms.py
@@ -1,8 +1,16 @@
 from django import forms
-
+from django .forms import ModelForm
 from .models import Comment
+from .models import Post
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
+
+class postform(forms.ModelForm):
+    class Meta:
+        model=Post
+        fields= "__all__"
+
+
