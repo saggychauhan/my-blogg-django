4c3b5a77a309e1403bc89b7f5c70f56eb60b4a36
saggychauhan <96346937+saggychauhan@users.noreply.github.com>
2022-01-28 13:27:29 +0530

    Add files via upload

--- myblogg/settings.py
+++ myblogg/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogg'
+    'blogg',
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- myblogg/urls.py
+++ myblogg/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from blogg.views import frontpage,post_detail,addpost
+from blogg import views
 
-from blogg.views import frontpage, post_detail
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('addpost/', addpost, name='addpost'),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls') ),
     path('admin/', admin.site.urls),
     path('<slug:slug>/', post_detail, name='post_detail'),
-]
+    path('<slug:slug>/delete_post',views.delete_post,name='delete_post'),
+    path('<slug:slug>/update_post',views.update_post,name='update_post')
 
+]
